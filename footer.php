47fde785a82b8228de0c0a3fcdbfc465b56f7d02
Norman Manantan <normanmanantan@gmail.com>
2014-06-02 16:52:01 +0800

    Added wp_footer()

--- footer.php
+++ footer.php
@@ -1,15 +1,11 @@
 	<section class="container-fluid footer">
 			<div class="row">
-				<div class="container-fluid">
-					<div class="col-md-3">WIDGET 1</div>
-					<div class="col-md-3">WIDGET 2</div>
-					<div class="col-md-3">WIDGET 3</div>
-					<div class="col-md-3">WIDGET 4</div>
-				</div>
+				<?php get_sidebar('footer'); ?>
 			</div>
 			<div class="row">
 				<div class="container-fluid">Blankslate Theme</div>
 			</div>
 		</section>
+	<?php wp_footer(); ?>
 	</body>
 </html>
