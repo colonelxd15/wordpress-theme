034702a4a63315f5f3b9d4ec9aed6e924e7a26fc
Norman Manantan <normanmanantan@gmail.com>
2014-06-02 16:50:30 +0800

    Template working but no permalink to individual post

--- Templates/fullwidth-bothsidebar.php
+++ Templates/fullwidth-bothsidebar.php
@@ -1,25 +1,27 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="../style.css">
-	</head>
-	<body>
-		<section class="container-fluid header">
-			<header class="header-info">
-				<div class="logo-placeholder">
-					<img src="logo.png">
-				</div>
-				<p>Site Title Place Here</p>
-				<p>Site Tagline Place Here</p>
-			</header>
-		</section>
-		<section class="container-fluid contents">
-			<div class="col-md-2 contents">SIDEBAR LEFT</div>
-			<div class="col-md-8 contents">
-				<h3>Title</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			</div>
-			<div class="col-md-2 contents">SIDEBAR RIGHT</div>
-		</section>
-		<section class="container-fluid footer">FOOTER</section>
-	</body>
-</html>
+<?php 
+/*
+Template Name: Full Width Page With Left and Right Sidebars
+ */
+get_header(); ?>
+	<section class="container-fluid contents">
+		<div class="col-md-2 contents">
+			<?php get_sidebar(); ?>
+		</div>
+		<div  class="col-md-8 contents">
+			<?php while(have_posts()): the_post(); ?>
+				<article>
+					<h3 class="entry-title"><?php the_title(); ?></h3>
+					<div class="entry-content"><?php the_content(); ?></div>
+					<?php 
+						if(comments_open() || get_comments_number()){
+							comments_template();
+						}
+					?>
+				</article>
+			<?php endwhile; ?>
+		</div>
+		<div class="col-md-2 contents">
+			<?php get_sidebar('right'); ?>
+		</div>
+	</section>
+<?php get_footer(); ?>
--- Templates/fullwidth-threecolumn.php
+++ Templates/fullwidth-threecolumn.php
@@ -1,51 +1,19 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="../style.css">
-	</head>
-	<body>
-	<div class="navbar navbar-default" role="navigation">
-	    <div class="container-fluid">
-	      <div class="navbar-header">
-	        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-	          <span class="icon-bar"></span>
-	          <span class="icon-bar"></span>
-	          <span class="icon-bar"></span>
-	        </button>
-	        <header class="header-info">
-				<div class="logo-placeholder">
-					<img src="">
-				</div>
-		        <p>Site Title Place Here</p>
-				<p>Site Tagline Place Here</p>
-			</header>
-	      </div>
-	      <div class="navbar-collapse collapse">
-	      <ul class="nav navbar-nav navbar-right">
-	          <li><a href="">1</a></li>
-	          <li><a href="">2</a></li>
-	          <li><a href="">3</a></li>
-	      </ul>
-	    </div>
-	  </div>
-	</div>
-		<section class="container-fluid contents">
-			<div class="col-md-4 contents">
-				<h3>Title</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			</div>
-			<div class="col-md-4 contents">
-				<h3>Title</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			</div>
-			<div class="col-md-4 contents">
-				<h3>Title</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			</div>
-		</section>
-		<section class="container-fluid footer">
-			<div class="row">
-				<div class="container-fluid">Blankslate Theme</div>
-			</div>
-		</section>
-	</body>
-</html>
+<?php 
+/*
+Template Name: Full Width Page (Three Columns)
+*/
+get_header(); ?>
+	<section class="container-fluid contents">
+		<?php while(have_posts()): the_post(); ?>
+			<article class="col-md-4">
+				<h3 class="entry-title"><?php the_title(); ?></h3>
+				<div class="entry-content"><?php the_content(); ?></div>
+				<!--<?php 
+					//if(comments_open() || get_comments_number()){
+					//	comments_template();
+					//}
+				?>-->
+			</article>
+		<?php endwhile; ?>
+	</section>
+<?php get_footer(); ?>
