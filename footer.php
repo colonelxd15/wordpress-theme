e6fabd3a02bbd7e20d898a00c2f5b2ff39f55ef9
Norman Manantan <normanmanantan@gmail.com>
2014-06-02 13:35:37 +0800

    Modified footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
-<section class="container-fluid footer">FOOTER</section>
+	<section class="container-fluid footer">
+			<div class="row">
+				<div class="container-fluid">
+					<div class="col-md-3">WIDGET 1</div>
+					<div class="col-md-3">WIDGET 2</div>
+					<div class="col-md-3">WIDGET 3</div>
+					<div class="col-md-3">WIDGET 4</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="container-fluid">Blankslate Theme</div>
+			</div>
+		</section>
 	</body>
 </html>
