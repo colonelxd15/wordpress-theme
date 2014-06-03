fbbba3b90aa43b35a49f5eebcae2474dd08f8fd3
Norman Manantan <normanmanantan@gmail.com>
2014-06-03 18:17:40 +0800

    Change container-fluid to container

--- featured_content.php
+++ featured_content.php
@@ -1,9 +1,12 @@
 <?php query_posts('category_name=Featured Contents')?>
-<div class="row">
+	<!-- <div class="container-fluid"> -->
+	<h2>Featured Contents</h2>
 	<?php while(have_posts()): the_post(); ?>
-	<a href="<?php the_permalink()?>"><?php the_title('<h3>','</h3>');?></a>
-	<p>
-		<?php the_content(); ?>
-	</p>
+		<article>
+		<a href="<?php the_permalink()?>"><?php the_title('<h3 class="entry-title">','</h3>');?></a>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+		</article>
 	<?php endwhile; ?>
-</div>
+<!-- </div> -->
