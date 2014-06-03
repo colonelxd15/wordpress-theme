2c10275fafcf4e2dab219f8020679130bdb99b58
Norman Manantan <normanmanantan@gmail.com>
2014-06-03 11:47:46 +0800

    Added single post template

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+	<section class="container contents">
+		<div class="row">
+			<div class="col-md-3 contents">
+				<?php get_sidebar(); ?>
+			</div>
+			<div class="col-md-9 contents">
+				<?php while(have_posts()): the_post() ?>
+					<article>
+						<?php the_title('<h3 class="entry-title">','</h3>'); ?>
+						<?php the_content('<div class="entry-content">', '</div>'); ?>
+						<?php 
+							if(comments_open() || get_comments_number()){
+								comments_template();
+							}
+						?>
+					</article>
+				<?php endwhile; ?>
+			</div>
+		</div>
+	</section>
+<?php get_footer(); ?>
