cd5b05c27c25bee99f12d1295928de3e98d0e121
Arvey Jimenez <arve505@gmail.com>
2014-06-03 18:14:54 +0800

    Textbox is modified.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function($) {
-    $('#s').val('').prop("placeholder","Search...");
+  
+    $('input[id$="s"]').attr('placeholder', 'Search');
+
+ 	$.each($('input, textarea'), function(index, value) {
+		$(this).data('holder', $(this).attr('placeholder'));
+	});
+
+    $('input, textarea').focusin(function(){
+        $(this).attr('placeholder','');
+    });
+
+    $('input, textarea').focusout(function(){
+        $(this).attr('placeholder', $(this).data('holder'));
+    });
+
 });
+
+
