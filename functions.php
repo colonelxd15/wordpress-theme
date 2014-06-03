fa159a0093f4a518e9f8f4b75edc542ff88693a7
PatzMatias <patzmatias@gmail.com>
2014-06-03 13:35:34 +0800

    finished navbar

--- functions.php
+++ functions.php
@@ -22,9 +22,42 @@
 			'before_title' => '<h3 class="widget-title">',
 			'after_title' => '</h3>'));
 	}
+	//Register and get jQuery from GoogleCDN
+	function jqueryjs(){
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', false, '1.11.1',true); //loads to footer
+		wp_enqueue_script('jquery');
+	}
+
+	//Register local Bootstrap
 	function wpt_register_js() {
-	    wp_register_script('jquery.bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery');
+	    wp_register_script('jquery.bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, '3.1.1', true); //loads to footer
 	    wp_enqueue_script('jquery.bootstrap.min');
 	}
+	
+	add_action( 'init', 'jqueryjs' );
 	add_action( 'init', 'wpt_register_js' );
+
+	register_nav_menus( array(
+	    'primary' => __( 'Primary Menu', 'wordpress-theme' ),
+	) );
+
+
+
+	require_once('wp_bootstrap_navwalker.php');
+
+	function menu_attr(){  
+		wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+        		'container_id'      => 'navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav navbar-right',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
+	}
+
 ?>
