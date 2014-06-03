4ff87029963df277276754b771ca98f9de5bcc33
Norman Manantan <normanmanantan@gmail.com>
2014-06-03 15:40:28 +0800

    Change container to container-fluid

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<section class="container contents">
+	<section class="container-fluid contents">
 		<div class="row">
 			<div class="col-md-3 contents">
 				<?php get_sidebar(); ?>
@@ -7,12 +7,14 @@
 			<div class="col-md-9 contents">
 				<?php while(have_posts()): the_post() ?>
 					<article>
-						<?php the_title('<h3 class="entry-title">','</h3>'); ?>
+						<a href="<?php the_permalink(); ?>">
+							<?php the_title('<h3 class="entry-title">','</h3>'); ?>
+						</a>
 						<?php the_content('<div class="entry-content">', '</div>'); ?>
 						<?php 
-							if(comments_open() || get_comments_number()){
-								comments_template();
-							}
+							// if(comments_open() || get_comments_number()){
+							// 	comments_template();
+							// }
 						?>
 					</article>
 				<?php endwhile; ?>
