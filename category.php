c937f248d8177abdacbc7c5b8416b4e4fa0663c4
Norman Manantan <normanmanantan@gmail.com>
2014-06-05 10:47:53 +0800

    Added Category.php/Fixed CSS Calendar widget

---
+++ category.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<section class="container-fluid contents">
+	<div class="col-md-3 contents">
+		<?php get_sidebar(); ?>
+	</div>
+	<div class="col-md-9 contents">
+	<?php 
+		if(is_front_page()) 
+		{
+			get_template_part('featured_content');
+		}
+	?>
+	<?php  
+		while(have_posts()): the_post() 
+	?>
+			<article>
+				<a href="<?php the_permalink(); ?>">
+					<?php the_title('<h3 class="entry-title">','</h3>'); ?>
+				</a>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</article>
+		<?php endwhile; ?>
+	</div>
+</section>
+<?php get_footer(); ?>
